bdcc1966ef563015c1e377e94461618964531b11
Yasin Şenel <yasinsenel3437@gmail.com>
2023-01-01 00:48:55 +0300

    Model Class Adding

---
+++ app/src/main/java/com/yasinsenel/weatherforecast/model/Sys.kt
@@ -0,0 +1,7 @@
+package com.yasinsenel.weatherforecast.model
+
+data class Sys(
+    val country: String,
+    val sunrise: Int,
+    val sunset: Int
+)
---
+++ app/src/main/java/com/yasinsenel/weatherforecast/model/Wind.kt
@@ -0,0 +1,7 @@
+package com.yasinsenel.weatherforecast.model
+
+data class Wind(
+    val deg: Int,
+    val gust: Double,
+    val speed: Double
+)
