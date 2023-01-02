46ae6c617f813c56342ec10176ebd6a61a5f8d2a
Yasin Şenel <yasinsenel3437@gmail.com>
2023-01-03 02:29:09 +0300

    model fix

--- app/src/main/java/com/yasinsenel/weatherforecast/service/WeatherAPI.kt
+++ app/src/main/java/com/yasinsenel/weatherforecast/service/WeatherAPI.kt
@@ -1,11 +1,11 @@
 package com.yasinsenel.weatherforecast.service
 
-import com.yasinsenel.weatherforecast.model.model2.WeatherResponseModel
+import com.yasinsenel.weatherforecast.model.WeatherResponseModel
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherAPI {
 
-    @GET("forecast.json?key=8230a509c28546efbe6220602230201&days=7&aqi=no&alerts=no")
+    @GET("forecast.json?key=8230a509c28546efbe6220602230201&days=5&aqi=yes&alerts=yes")
     suspend fun getData(@Query("q") cityName : String?) : WeatherResponseModel
 }
